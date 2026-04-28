a4a320c2d4c3cd824664e3175c802118e1856405
Nathan Dobson <nadobson@mac.com>
2026-04-27 17:09:10 -0700

    Fix release test.

--- release/release_test.sh
+++ release/release_test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 set -u
-(cd firmware; cargo hack check --feature-powerset --no-dev-deps --depth 2)
-(cd firmware; cargo test)
+(cd firmware; cargo hack check --feature-powerset --no-dev-deps --depth 2 -p firmware)
+(cd firmware; cargo test -p firmware)
 (cd common; cargo test)
 (cd native-client; cargo hack check --feature-powerset --no-dev-deps)
 (cd native-client; cargo test)
